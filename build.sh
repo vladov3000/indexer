3d3e0757fa7e1d482ac498cc0e49aabd0b0921ec
Vladimir Ovechkin <vladov3000@gmail.com>
2024-11-11 23:13:29 -0800

    Add linux build.

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    CC=g++
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    CC=clang
+else
+    echo "Unknown operating system \"$OSTYPE\"."
+    exit 1
+fi
+
 mkdir -p build
-clang++ -g -std=c++20 code/main.cpp -o build/indexer
-clang++ -g -std=c++20 code/test.cpp -o build/test
+"$CC" -g -std=c++20 code/main.cpp -o build/indexer
+"$CC" -g -std=c++20 code/test.cpp -o build/test
